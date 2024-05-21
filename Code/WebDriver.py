f281d732ae580b02cbfb1f685a98bfe8d948dc83
Egor <lobanow.egor@outlook.com>
2024-05-21 03:22:06 +0300

    даже не знаю чо

--- Code/WebDriver.py
+++ Code/WebDriver.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def randomwiki():
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get('https://ru.wikipedia.org/')
     element = driver.find_element(By.LINK_TEXT, 'Случайная статья')
     element.click()
