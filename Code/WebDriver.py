0da0da5c9a60191518f596f4a2e4bbaeb15e48de
Egor <lobanow.egor@outlook.com>
2024-05-22 22:46:21 +0300

    Добавил поддержку браузеров Firefox, Google Chrome, Microsoft Edge и решил проблему с тем, что Chrome и Edge закрывались сразу после выполнения кода.

--- Code/WebDriver.py
+++ Code/WebDriver.py
@@ -1,8 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import platform
+import logging
+
 
 def randomwiki():
-    driver = webdriver.Edge()
+    browser = find_default_browser()
+    driver_class = getattr(webdriver, browser)
+    driver = driver_class()
     driver.get('https://ru.wikipedia.org/')
     element = driver.find_element(By.LINK_TEXT, 'Случайная статья')
     element.click()
+    while (True):
+        pass
+def find_default_browser():
+    osPlatform = platform.system()
+
+    if osPlatform == 'Windows':
+
+        try:
+            from winreg import HKEY_CLASSES_ROOT, HKEY_CURRENT_USER, OpenKey, QueryValueEx
+
+            with OpenKey(HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice') as regkey:
+                browser_choice = QueryValueEx(regkey, 'ProgId')[0]
+
+        except Exception:
+            logging.error('Failed to look up default browser')
+
+    if browser_choice[0] == 'C':
+        browser_choice = browser_choice.split('HTML')[0]
+    elif browser_choice[0] == 'F':
+        browser_choice = browser_choice.split('URL')[0]
+    elif browser_choice[0] == 'M':
+        browser_choice = browser_choice[2:6]
+
+    return browser_choice
